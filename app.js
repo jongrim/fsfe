e757e409f44e2b0a4a060ea3ac60a8eabfee8ee0
Jonathan <jonathan.grim@ware2go.co>
2020-01-03 11:20:19 -0500

    teapot

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.listen(port, () => console.log(`App listening on port ${port}!`)); 
+app.get("/teapot", (req, res) => {
+  res.status(418);
+  res.set("X-TEAPOT", "true");
+  res.send("I prefer coffee");
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
